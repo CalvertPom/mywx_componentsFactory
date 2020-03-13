c961bc028f1051d7279e11d20918b91d6c086471
CalvertPom <calvertpom@qq.com>
2020-03-13 21:25:41 +0800

    进度条控件
    进度条控件

---
+++ components/progress/index.js
@@ -0,0 +1,19 @@
+Component({
+    externalClasses: ['i-class'],
+
+    properties: {
+        percent: {
+            type: Number,
+            value: 0
+        },
+        
+        strokeWidth: {
+            type: Number,
+          value: 32
+        },
+        hideInfo: {
+            type: Boolean,
+            value: false
+        }
+    }
+});
